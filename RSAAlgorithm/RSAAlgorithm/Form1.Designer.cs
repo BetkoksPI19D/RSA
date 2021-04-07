d1da592edbfda4fbf164b371aa39c78a3e8f8b0c
BetkoksPI19D <65999327+BetkoksPI19D@users.noreply.github.com>
2021-04-07 19:43:10 +0200

    Add files via upload

--- RSAAlgorithm/RSAAlgorithm/Form1.Designer.cs
+++ RSAAlgorithm/RSAAlgorithm/Form1.Designer.cs
@@ -28,13 +28,276 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pTextBox = new System.Windows.Forms.TextBox();
+            this.qTextBox = new System.Windows.Forms.TextBox();
+            this.PrimaryTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.EncryptButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.DecryptButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.eTextBox = new System.Windows.Forms.TextBox();
+            this.DTextBox = new System.Windows.Forms.TextBox();
+            this.nTextBox = new System.Windows.Forms.TextBox();
+            this.FiTextBox = new System.Windows.Forms.TextBox();
+            this.DecryptedResultBox = new System.Windows.Forms.TextBox();
+            this.EncryptedResultBox = new System.Windows.Forms.RichTextBox();
+            this.EncryptedTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "p=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(138, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "q=";
+            // 
+            // pTextBox
+            // 
+            this.pTextBox.Location = new System.Drawing.Point(62, 41);
+            this.pTextBox.MaxLength = 3276764;
+            this.pTextBox.Name = "pTextBox";
+            this.pTextBox.Size = new System.Drawing.Size(70, 22);
+            this.pTextBox.TabIndex = 2;
+            // 
+            // qTextBox
+            // 
+            this.qTextBox.Location = new System.Drawing.Point(168, 41);
+            this.qTextBox.MaxLength = 3276765;
+            this.qTextBox.Name = "qTextBox";
+            this.qTextBox.Size = new System.Drawing.Size(75, 22);
+            this.qTextBox.TabIndex = 3;
+            // 
+            // PrimaryTextBox
+            // 
+            this.PrimaryTextBox.Location = new System.Drawing.Point(120, 184);
+            this.PrimaryTextBox.MaxLength = 3276765;
+            this.PrimaryTextBox.Name = "PrimaryTextBox";
+            this.PrimaryTextBox.Size = new System.Drawing.Size(202, 22);
+            this.PrimaryTextBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Primary text:";
+            // 
+            // EncryptButton
+            // 
+            this.EncryptButton.Location = new System.Drawing.Point(247, 212);
+            this.EncryptButton.Name = "EncryptButton";
+            this.EncryptButton.Size = new System.Drawing.Size(75, 28);
+            this.EncryptButton.TabIndex = 6;
+            this.EncryptButton.Text = "Encrypt";
+            this.EncryptButton.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 254);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Encrypted text:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(430, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Encrypted text:";
+            // 
+            // DecryptButton
+            // 
+            this.DecryptButton.Location = new System.Drawing.Point(654, 194);
+            this.DecryptButton.Name = "DecryptButton";
+            this.DecryptButton.Size = new System.Drawing.Size(75, 28);
+            this.DecryptButton.TabIndex = 12;
+            this.DecryptButton.Text = "Decrypt";
+            this.DecryptButton.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(430, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Result text:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(32, 101);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "e=";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(32, 129);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(24, 17);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "n=";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(32, 157);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(27, 17);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Fi=";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(220, 104);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(26, 17);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "D=";
+            // 
+            // eTextBox
+            // 
+            this.eTextBox.Location = new System.Drawing.Point(62, 101);
+            this.eTextBox.MaxLength = 3276765;
+            this.eTextBox.Name = "eTextBox";
+            this.eTextBox.Size = new System.Drawing.Size(70, 22);
+            this.eTextBox.TabIndex = 18;
+            // 
+            // DTextBox
+            // 
+            this.DTextBox.Location = new System.Drawing.Point(252, 101);
+            this.DTextBox.MaxLength = 3276765;
+            this.DTextBox.Name = "DTextBox";
+            this.DTextBox.Size = new System.Drawing.Size(70, 22);
+            this.DTextBox.TabIndex = 19;
+            // 
+            // nTextBox
+            // 
+            this.nTextBox.Location = new System.Drawing.Point(62, 129);
+            this.nTextBox.MaxLength = 3276765;
+            this.nTextBox.Name = "nTextBox";
+            this.nTextBox.Size = new System.Drawing.Size(70, 22);
+            this.nTextBox.TabIndex = 20;
+            // 
+            // FiTextBox
+            // 
+            this.FiTextBox.Location = new System.Drawing.Point(62, 156);
+            this.FiTextBox.MaxLength = 3276765;
+            this.FiTextBox.Name = "FiTextBox";
+            this.FiTextBox.Size = new System.Drawing.Size(70, 22);
+            this.FiTextBox.TabIndex = 21;
+            // 
+            // DecryptedResultBox
+            // 
+            this.DecryptedResultBox.Location = new System.Drawing.Point(514, 236);
+            this.DecryptedResultBox.MaxLength = 3276765;
+            this.DecryptedResultBox.Name = "DecryptedResultBox";
+            this.DecryptedResultBox.Size = new System.Drawing.Size(215, 22);
+            this.DecryptedResultBox.TabIndex = 11;
+            // 
+            // EncryptedResultBox
+            // 
+            this.EncryptedResultBox.Location = new System.Drawing.Point(120, 254);
+            this.EncryptedResultBox.Name = "EncryptedResultBox";
+            this.EncryptedResultBox.Size = new System.Drawing.Size(202, 96);
+            this.EncryptedResultBox.TabIndex = 22;
+            this.EncryptedResultBox.Text = "";
+            // 
+            // EncryptedTextBox
+            // 
+            this.EncryptedTextBox.Location = new System.Drawing.Point(538, 41);
+            this.EncryptedTextBox.Name = "EncryptedTextBox";
+            this.EncryptedTextBox.Size = new System.Drawing.Size(191, 147);
+            this.EncryptedTextBox.TabIndex = 23;
+            this.EncryptedTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(753, 375);
+            this.Controls.Add(this.EncryptedTextBox);
+            this.Controls.Add(this.EncryptedResultBox);
+            this.Controls.Add(this.FiTextBox);
+            this.Controls.Add(this.nTextBox);
+            this.Controls.Add(this.DTextBox);
+            this.Controls.Add(this.eTextBox);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.DecryptButton);
+            this.Controls.Add(this.DecryptedResultBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.EncryptButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.PrimaryTextBox);
+            this.Controls.Add(this.qTextBox);
+            this.Controls.Add(this.pTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox pTextBox;
+        private System.Windows.Forms.TextBox qTextBox;
+        private System.Windows.Forms.TextBox PrimaryTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button EncryptButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button DecryptButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox eTextBox;
+        private System.Windows.Forms.TextBox DTextBox;
+        private System.Windows.Forms.TextBox nTextBox;
+        private System.Windows.Forms.TextBox FiTextBox;
+        private System.Windows.Forms.TextBox DecryptedResultBox;
+        private System.Windows.Forms.RichTextBox EncryptedResultBox;
+        private System.Windows.Forms.RichTextBox EncryptedTextBox;
     }
 }
 
--- RSAAlgorithm/RSAAlgorithm/Form1.cs
+++ RSAAlgorithm/RSAAlgorithm/Form1.cs
@@ -4,17 +4,150 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Diagnostics;
+using System.IO;
 
 namespace RSAAlgorithm
 {
     public partial class Form1 : Form
     {
+        Encoding encoding = Encoding.GetEncoding("437");
+        List<int> encryptedMsg;
+        int p;
+        int q;
+        int D = 0;
+        int e = 0;
+        int n = 0;
+        int Fi = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private int getN()
+        {
+            p = int.Parse(pTextBox.Text);
+            q = int.Parse(qTextBox.Text);
+
+            n = p * q;
+
+            return n;
+        }
+
+        private int getFi()
+        {
+            p = int.Parse(pTextBox.Text);
+            q = int.Parse(qTextBox.Text);
+            Fi = (p - 1) * (q - 1);
+
+            return Fi;
+        }
+
+        private static List<int> getPrimeNum(int num)
+        {
+            List<int> primeNum = new List<int>();
+            for (int i = 1; i <= num; i++)
+            {
+                int counter = 0;
+                for (int j = 1; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        counter++;
+                        break;
+                    }
+                }
+
+                if (counter == 0 && i != 1)
+                {
+                    Console.WriteLine("{0} ", i);
+                    primeNum.Add(i);
+                }
+            }
+            return primeNum;
+        }
+        private static int DBD(int x, int y)
+        {
+
+            if (x == 0)
+                return y;
+            if (y == 0)
+                return x;
+
+            if (x == y)
+                return x;
+
+            if (x > y)
+                return DBD(x - y, y);
+            return DBD(x, y - x);
+
+        }
+        private int getE()
+        {
+            Fi = getFi();
+            List<int> primeList = getPrimeNum(Fi);
+
+            foreach (int num in primeList)
+            {
+                if (DBD(num, Fi) == 1)
+                {
+                    e = num;
+                    break;
+                }
+            }
+
+            return e;
+        }
+        private int privateKey()
+        {
+            e = getE();
+            Fi = getFi();
+            while (D * e % Fi != 1)
+            {
+                D++;
+            }
+            return D;
+        }
+        private List<int> publicKey()
+        {
+            e = 0;
+            p = int.Parse(pTextBox.Text);
+            q = int.Parse(qTextBox.Text);
+            int n = getN();
+            n = p * q;
+
+            Fi = (p - 1) * (q - 1);
+
+
+            List<int> primeList = getPrimeNum(Fi);
+
+            foreach (int num in primeList)
+            {
+                if (DBD(num, Fi) == 1)
+                {
+                    e = num;
+                    break;
+                }
+            }
+
+            List<int> publicKey = new List<int>();
+            publicKey.Add(n);
+            publicKey.Add(e);
+
+            foreach (int a in publicKey)
+            {
+                using (StreamWriter writer = new StreamWriter(@"C:\Users\njusC:\Users\njusp\OneDrive - Vilniaus kolegija\2 kursas\Informacijos Saugumas\RSA\key.txt", true))
+                {
+                    writer.WriteLine(a);
+                }
+            }
+
+            return publicKey;
         }
     }
 }
